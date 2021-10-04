ef8bd66bd74b0d1c94af8a00f6ff8a8bff49f9d0
Julia <julianorwill@gmail.com>
2021-10-04 17:30:34 +0300

    feat: add burger-menu and pictures in avif format

--- museum/index.js
+++ museum/index.js
@@ -1,17 +1,26 @@
-console.log(`1.Вёрстка валидная +10
-        2.Вёрстка семантическая +24.(<header>, <main>, <footer>, семь <section>, 
-        только один <h1>, шесть <h3>, два <nav>, три списка ul > li > a,
-        тринадцать кнопок button, три input type="radio", два input type="number",
-        два input type="range", для всех <img> указанb alt)
-        3.Вёрстка соответствует макету +45
-        4.Форма покупки билетов +22
-        5.Требования к css +18
-        6.Интерактивность, реализуемая через css +25`)
+console.log(`150/150
+1.Вёрстка соответствует макету. Ширина экрана 1024px +40
+2.Вёрстка соответствует макету. Ширина экрана 768px +40
+3.Вёрстка соответствует макету. Ширина экрана 420px +40
+4.Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки +6
+5.Совмещается адаптивная и респонсивная (резиновая) вёрстка +14
+6.На ширине экрана 1024рх и меньше реализовано адаптивное меню +12
+7.Оптимизация скорости загрузки страницы +4`)
+const burgerMenuImg = document.querySelector('.burger-menu-img')
+const burgerMenu = document.querySelector('.burger-menu')
 const ticketsForm = document.querySelector('.tickets__form')
 const buttonClose = document.querySelector('.booking-tickets__close')
 const buttonOpen = document.querySelector('.form__submit')
 const elemTicketContainer = document.querySelector('.booking-tickets-container')
 const elemTicketElem = document.querySelector('.booking-tickets')
+
+
+burgerMenuImg.addEventListener('click', () => { burgerMenuImg.classList.toggle('open')
+burgerMenu.classList.toggle('burger-menu-open') 
+document.querySelector('.welcome__text-container').classList.toggle('welcome__text-container_opacity')
+document.querySelector('.welcome').classList.toggle('welcome-hidden')
+});
+
 ticketsForm.addEventListener('submit', (event) => { event.preventDefault(); })
 elemTicketContainer.onclick = function (event) {
         let target = event.target;
